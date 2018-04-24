516f14ba531dd2397c33210322621f6a778b281e
Pychofan <35198199+pychofan@users.noreply.github.com>
2018-04-24 05:01:04 +0200

    make it work

--- github.php
+++ github.php
@@ -1,1 +1,9 @@
-<?php `git pull`;
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+shell_exec( ‘cd /home/cabox/workspace/ && git pull’ );
+}
+
+?>hi
